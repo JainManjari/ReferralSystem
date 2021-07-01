925422bb5a5dc0ff1a321baea35347e774fbf667
manjari jain <manjarijain98@gmail.com>
2021-07-01 17:22:48 +0530

    employee profile complete

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,4 +1,36 @@
-module.exports.home=function(req,res)
+let Employee=require('../models/employees');
+
+module.exports.home=async function(req,res)
 {
-    return res.render("home",{"title":"Referral Home"});
+    try
+    {
+        if(req.user)
+        {
+            let user=await Employee.findById(req.user._id).populate({
+                path:"referees",
+                options:{
+                    sort:"-createdAt"
+                }
+            });
+            if(!user.isEmployee)
+            {
+                return res.render("home",{"title":"Referral Home"});
+            }
+            else
+            {
+                return res.render("home",{
+                    "title":"Referral Home",
+                    "referees":user.referees,
+                    "totalReward":user.referees.length*100
+                });
+            }
+        }
+        return res.render("home",{"title":"Referral Home"});
+        
+    }
+    catch(err)
+    {
+        console.log("Error in loading home page "+err);
+        return;
+    }
 }
